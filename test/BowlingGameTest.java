60649b771d9aa565d6071d1d6272da2a21d4717b
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-04-09 22:25:20 +0900

    The first test

---
+++ test/BowlingGameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testGutterGame() {
+		Game g = new Game();
+	}
+	
+}
