66692017f9e9137317205ea8554454086a00e986
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-04-10 14:25:31 +0900

    The Second Test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -6,9 +6,29 @@ import org.junit.Test;
 
 public class BowlingGameTest {
 
-	@Test
-	public void testGutterGame() {
-		Game g = new Game();
+	protected Game g;
+	@Before
+	public void setUp() {
+		g = new Game();
 	}
 	
+	@Test
+	public void testGutterGame() {
+		rollMany(20,0);
+		
+		assertEquals(0,g.score());
+	}
+	
+	@Test
+	public void testAllOnes() {
+		rollMany(20, 1);
+		
+		assertEquals(20,g.score());
+	}
+
+	protected void rollMany(int n, int pins) {
+		for(int i=0;i<n;i++) {
+			g.roll(pins);
+		}
+	}
 }
